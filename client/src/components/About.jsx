1330a800df144fd8529aa7ae84faafb3e40432f6
IsaGuapo <isabela.guapo@gmail.com>
2022-01-09 22:04:35 +0100

    delete link tienda

---
+++ client/src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const About = () => {
+    return (
+    <section className="d-flex justify-content-center card text-white bg-primary mb-3">
+        <h3 className="card-header">Sobre Nosotros</h3>
+    <section className="card-body m-3">
+        <p className="card-text">Empresa dedicada a la venta y asesoramiento de materia de montaña y escalada</p>
+        <p className="card-text">Con mas de 35 años de experiencia en el sector</p>
+        <p className="card-text">Equipando con los materiales mñas sofisticados a los escaladores y montañistas del ambito nacional</p>
+        <p className="card-text">Nos aseguramos de que todos los productos que tenemos en venta sean testados por nuestro equipo de patrocinados</p>
+    </section>
+    </section>
+    )
+}
